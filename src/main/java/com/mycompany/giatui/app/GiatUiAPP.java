232c9ccc7b16ad3d7cd9153006ab7024cb695204
ADMIN <ADMIN@PC341>
2025-08-28 13:16:52 +0700

    APPGIATUI

---
+++ src/main/java/com/mycompany/giatui/app/GiatUiAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.giatui.app;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class GiatUiAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
